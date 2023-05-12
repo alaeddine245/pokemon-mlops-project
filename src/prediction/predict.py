c228d7e90b4d1e9e65abedb387f0bf4625c09fb6
Alaeddine <alaeddinefattouch@insat.u-carthage.tn>
2023-05-12 18:47:56 +0100

    added predict script

---
+++ src/prediction/predict.py
@@ -0,0 +1,10 @@
+from tensorflow.keras.utils import load_img, img_to_array
+
+def predict_image(model, image_path):
+  img = load_img(image_path, target_size=(img_width, img_height))
+  x = img_to_array(img)
+  x = np.expand_dims(x, axis=0)
+
+  images = np.vstack([x])
+  preds = model.predict(images, batch_size=10)
+  return preds
