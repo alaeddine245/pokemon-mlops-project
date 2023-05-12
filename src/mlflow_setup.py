7b57d27d91709a1a9403d99d937dcf0086e2a9a6
Wassim Henia <wassim.henia@maibornwolff.de>
2023-05-12 23:26:15 +0100

    changing url to mlflow on azure

--- src/mlflow_setup.py
+++ src/mlflow_setup.py
@@ -1,7 +1,7 @@
 import mlflow
 
 def setup_mlflow(experiment_name):
-    mlflow.set_tracking_uri("http://localhost:5000")  
+    mlflow.set_tracking_uri("http://mlops.uksouth.cloudapp.azure.com:5000")  
     mlflow.set_experiment(experiment_name)
     
 
