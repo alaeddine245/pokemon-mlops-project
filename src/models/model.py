d8a179cee84ca417f37392a4b5762aa891130585
Wassim Henia <wassim.henia@maibornwolff.de>
2023-05-12 21:24:29 +0100

    Revert "logging more metrics"
    This reverts commit 3480078217b20f08bca83e8a25174810fec722ec.

--- src/models/model.py
+++ src/models/model.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
+from keras.applications.vgg16 import VGG16
 from keras.applications.efficientnet_v2 import EfficientNetV2B2
-import keras
 
 
 def transfer_learning(model, number_classes, learning_rate):
@@ -9,7 +9,7 @@ def transfer_learning(model, number_classes, learning_rate):
   x=tf.keras.layers.Flatten()(model.output)
   pred=tf.keras.layers.Dense(number_classes, activation='softmax')(x)
   model=tf.keras.Model(inputs=model.input, outputs=pred)
-  model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate), loss='categorical_crossentropy', metrics=[keras.metrics.CategoricalAccuracy(), keras.metrics.Precision(), keras.metrics.Recall(), keras.metrics.F1Score, keras.metrics.AUC()])
+  model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate), loss='categorical_crossentropy', metrics=[keras.metrics.CategoricalAccuracy(), keras.metrics.Precision(), keras.metrics.Recall()])
   return model
 
 model=EfficientNetV2B2(input_shape=(224,224,3),weights='imagenet',include_top=False)
