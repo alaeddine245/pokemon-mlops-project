a5d9c527ed6e288f2cc33e43cb0e630d2a91c905
Bechir Brahem <bachbrahem@gmail.com>
2023-05-13 00:21:53 +0100

    fix mlflow.sklearn to mlflow.keras in train.py

--- src/training/train.py
+++ src/training/train.py
@@ -6,6 +6,6 @@ def train(model, train_ds, val_ds, epochs=6):
     mlflow.keras.log_model(model, "model")
     mlflow.keras.autolog()
     history=model.fit(train_ds,validation_data=val_ds,epochs=epochs)
-    mlflow.sklearn.log_model(model, "model")
+    mlflow.keras.log_model(model, "model")
     # mlflow.sklearn.save_model(model, modelpath)
     return model, history
