b5bb7216ea35750d893fe9e01de183fecc64845f
Wassim Henia <wassim.henia@maibornwolff.de>
2023-05-12 21:29:38 +0100

    fixing imports and adding more metrics

--- src/evaluation/evaluate.py
+++ src/evaluation/evaluate.py
@@ -8,10 +8,18 @@ def evaluate_model(model: tf.keras.Model, X_test: list, y_test: list):
         mlflow.keras.log_model(model, "model")
         
         # Evaluate the model on the test set
-        loss, accuracy = model.evaluate(X_test, y_test)
+        loss, accuracy, precision, recall, f1, auc = model.evaluate(X_test, y_test)
         print(f"Test loss: {loss:.4f}")
         print(f"Test accuracy: {accuracy:.4f}")
+        print(f"Test precision: {precision:.4f}")
+        print(f"Test recall: {recall:.4f}")
+        print(f"Test f1: {f1:.4f}")
+        print(f"Test auc: {auc:.4f}")
         
         # Log the evaluation metrics
         mlflow.log_metric("test_loss", loss)
         mlflow.log_metric("test_accuracy", accuracy)
+        mlflow.log_metric("test_precision", precision)
+        mlflow.log_metric("test_recall", recall)
+        mlflow.log_metric("test_f1", f1)
+        mlflow.log_metric("test_auc", auc)
